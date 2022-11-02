a30fa3916eea3c9c327d2c66264039658b71bcc9
Константин <zamaraevki@gmai.com>
2022-11-02 13:05:26 +0500

    7. Date. Отображение даты. [#310017]

--- src/main/java/ru/job4j/inheritance/StartUI.java
+++ src/main/java/ru/job4j/inheritance/StartUI.java
@@ -1,1 +1,15 @@
+package ru.job4j.inheritance;
 
+import ru.job4j.tracker.Item;
+
+import java.time.format.DateTimeFormatter;
+
+public class StartUI extends Item {
+
+    public static void main(String[] args) {
+        Item item = new Item();
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MMMM-EEEE-yyyy HH:mm:ss");
+        String currentDateTimeFormat = item.getCreated().format(formatter);
+        System.out.println("Текущие дата и время после форматирования: " + currentDateTimeFormat);
+    }
+}
---
+++ src/main/java/ru/job4j/tracker/Item.java
@@ -0,0 +1,43 @@
+package ru.job4j.tracker;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+public class Item {
+    private int id;
+    private String name;
+    private LocalDateTime created;
+
+    public Item() {
+        this.created = LocalDateTime.now();
+    }
+
+    public Item(int id) {
+        this.id = id;
+    }
+
+    public Item(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public LocalDateTime getCreated() {
+        return created;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
