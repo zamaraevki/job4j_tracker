ec77313efc38b8b74c7d8a82cee6c5d0304999ca
Константин <zamaraevki@gmai.com>
2022-11-03 10:18:01 +0500

    7. Date. Отображение даты. [#310017 #357178]

--- src/main/java/ru/job4j/tracker/Item.java
+++ src/main/java/ru/job4j/tracker/Item.java
@@ -6,10 +6,9 @@ import java.time.format.DateTimeFormatter;
 public class Item {
     private int id;
     private String name;
-    private LocalDateTime created;
+    private LocalDateTime created = LocalDateTime.now();
 
     public Item() {
-        this.created = LocalDateTime.now();
     }
 
     public Item(int id) {
