d325bafdb1c3bfe42d8f0f296c51f042b1e39945
Константин <zamaraevki@gmai.com>
2022-11-03 10:26:35 +0500

    3. Массивы и модели. [#174892 #357217]

--- src/main/java/ru/job4j/pojo/Library.java
+++ src/main/java/ru/job4j/pojo/Library.java
@@ -2,27 +2,24 @@ package ru.job4j.pojo;
 
 public class Library {
     public static void main(String[] args) {
-        Book book1 = new Book("Clean cod", 213);
-        Book book2 = new Book("The Da Vinci Code", 315);
-        Book book3 = new Book("War and Peace", 756);
-        Book book4 = new Book("The Great Gatsby", 459);
         Book[] books = new Book[4];
-        books[0] = book1;
-        books[1] = book2;
-        books[2] = book3;
-        books[3] = book4;
+        books[0] = new Book("Clean code", 213);
+        books[1] = new Book("The Da Vinci Code", 315);
+        books[2] = new Book("War and Peace", 756);
+        books[3] = new Book("The Great Gatsby", 459);
         for (int i = 0; i < books.length; i++) {
             System.out.println(books[i].getName() + " - " + books[i].getPages());
         }
         System.out.println("swap 0 and 3");
-        books[0] = book4;
-        books[3] = book1;
+        Book temp = books[0];
+        books[0] = books[3];
+        books[3] = temp;
         for (int i = 0; i < books.length; i++) {
             System.out.println(books[i].getName() + " - " + books[i].getPages());
         }
-        System.out.println("print only Clean cod");
+        System.out.println("print only Clean code");
         for (int i = 0; i < books.length; i++) {
-            if (books[i].getName().equals("Clean cod")) {
+            if ("Clean code".equals(books[i].getName())) {
             System.out.println(books[i].getName() + " - " + books[i].getPages());
             }
         }
