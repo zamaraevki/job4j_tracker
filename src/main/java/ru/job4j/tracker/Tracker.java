a8d256d86ff05eb830151d244a055cf4b7f96299
Константин <zamaraevki@gmai.com>
2022-11-09 10:25:30 +0500

    8. Что такое валидация? [#246864 #358967]

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -52,7 +52,6 @@ public class Tracker {
             if (rsl) {
                 item.setId(id);
                 items[index] = item;
-                rsl = true;
             }
         return rsl;
     }
@@ -65,7 +64,6 @@ public class Tracker {
             System.arraycopy(items, index + 1, items, index, size - index - 1);
             items[size - 1] = null;
             size--;
-            rsl = true;
             }
         return rsl;
     }
