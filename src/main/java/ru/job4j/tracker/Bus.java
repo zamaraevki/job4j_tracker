f9ef2b1578c2f04b3730456df9a8669f11cd5c22
Константин <zamaraevki@gmai.com>
2022-11-29 10:53:29 +0500

    3. Интерфейс Input [#181092]

---
+++ src/main/java/ru/job4j/tracker/Bus.java
@@ -0,0 +1,18 @@
+package ru.job4j.tracker;
+
+public class Bus implements Transport {
+    @Override
+    public void drive() {
+
+    }
+
+    @Override
+    public void passengers(int amount) {
+
+    }
+
+    @Override
+    public int refuel(int fuel) {
+        return 0;
+    }
+}
