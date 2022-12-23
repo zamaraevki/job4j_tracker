aaf11d44ca880af1b3d041fbec11cd1de249a34e
Константин <zamaraevki@gmai.com>
2022-12-23 11:55:12 +0500

    2. Очередь с приоритетом на LinkedList [#41670 #373622]

--- src/main/java/ru/job4j/search/PriorityQueue.java
+++ src/main/java/ru/job4j/search/PriorityQueue.java
@@ -16,6 +16,8 @@ public class PriorityQueue {
         for (Task element : tasks) {
             if (element.getPriority() <= task.getPriority()) {
                 index++;
+            } else {
+                break;
             }
         }
         this.tasks.add(index, task);
